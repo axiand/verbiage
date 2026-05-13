e3e0cc7ebed2af44df9fc54b09a20ff74542c0e3
Axi <98420477+axiand@users.noreply.github.com>
2026-05-14 00:53:50 +0200

    cabinet: beginnings of sync to disk

--- bag-test.js
+++ bag-test.js
@@ -1,23 +1,14 @@
 const { Bag } = require('./lib/cabinet/Bag.js')
+const { join } = require("path")
 
-let bag = new Bag()
+async function main() {
 
-for (i = 0; i < 1_000_000; i++) {
-    //bag.set({ id: i, asdf: String.fromCharCode(...new Array(16).fill(1).map(() => { return Math.round(Math.random() * 16) + 65 })) })
-    bag.set({ id: i, asdf: Math.random() })
+    let bag = await Bag.fromFile(join(__dirname, "/bag.dat"))
+
+    for (i = 0; i < 8; i++) {
+        bag.set({ id: i, asdf: String.fromCharCode(...new Array(16).fill(1).map(() => { return Math.round(Math.random() * 16) + 65 })) })
+    }
+
 }
 
-//console.log(bag)
-
-console.time("query")
-
-let rs = bag
-    .query()
-    .order("asdf", false)
-    .limit(8)
-    .lesserThan("asdf", 0.6)
-    .greaterThan("asdf", 0.34)
-    .getMany()
-
-console.timeLog("query")
-console.log(rs)
+main()
